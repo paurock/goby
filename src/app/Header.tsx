855642bddb3c17e6fe755a3c45140ae35b485b64
paurock <tselenkor@gmail.com>
2022-06-14 15:48:55 +0300

    logo started

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,19 +1,27 @@
-import Link from "next/link";
-import React, { useState } from "react";
-import { useRouter } from "next/router";
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 
-import { Container, Flex } from "@chakra-ui/react";
+import { Container, Flex } from '@chakra-ui/react';
+import Image from 'next/image';
 
 export const Header = () => {
   const { route } = useRouter();
   return (
     <Container
-      maxW={["none", "750px", "970px", "1100px", "1440px"]}
-      px={["16px", "16px", "20px", "120px"]}
+      maxW={['none', '750px', '970px', '1440px', '1440px']}
+      px={['16px', '16px', '20px', '120px']}
+      border={'1px solid red'}
+      paddingStart={0}
     >
-      <Flex>
-        <h1>Header</h1>
-      </Flex>
+      <Image src="/images/logoPic.png" alt="Logo" width={40} height={40} />
+      <Image
+        src="/images/logoText.png"
+        alt="Logo"
+        width={94.39}
+        height={33.33}
+      />
+      {/* <h1 style={{ border: '1px solid green' }}>Header</h1> */}
     </Container>
   );
 };
