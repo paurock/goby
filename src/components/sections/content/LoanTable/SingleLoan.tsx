72aeba54082611f5e1e9d70209ab26d28d4b697a
paurock <tselenkor@gmail.com>
2022-06-30 18:51:39 +0300

    loan table image size padding fix

--- src/components/sections/content/LoanTable/SingleLoan.tsx
+++ src/components/sections/content/LoanTable/SingleLoan.tsx
@@ -21,12 +21,7 @@ import image from "/src/app/assets/square_img_150x150.png";
 
 export const LoanTable = ({ status, getAsset = false }: loanTableType) => {
   return (
-    <Flex
-      w="full"
-      pt="24px"
-      pr={[0, 0, 0, "195px"]}
-      flexWrap={["wrap", "nowrap"]}
-    >
+    <Flex w="full" pt="24px" flexWrap={["wrap", "nowrap"]}>
       {/* IMAGE  */}
       <Box w="full" maxW={["100%", "150px"]} m={["24px", 0]}>
         <Img alt="image" width="100%" objectFit="cover" src={image.src} />
