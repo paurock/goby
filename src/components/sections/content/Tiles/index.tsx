9f15b3fccaafe5d25a7f810b372b634ccf9df471
paurock <tselenkor@gmail.com>
2022-07-13 11:02:48 +0300

    Collateral page: added skeletons

--- src/components/sections/content/Tiles/index.tsx
+++ src/components/sections/content/Tiles/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Flex, SimpleGrid } from '@chakra-ui/react';
+import { Flex, SimpleGrid, Skeleton } from '@chakra-ui/react';
 import { Img } from 'shared';
 import image1 from '/src/app/assets/rectangle_240x229.png';
 import imgage2 from '/src/app/assets/rectangle_240x319.png';
@@ -15,12 +15,21 @@ export default function Tiles(): JSX.Element {
         maxW="1050px"
       >
         <SimpleGrid w="full" columns={[1, 2, 3, 4]} gap="30px" mt="24px">
-          <Img src={image1.src} w="full" maxH="229px" />
-          <Img src={image1.src} w="full" maxH="229px" />
-          <Img src={imgage2.src} w="full" maxH="319px" />
-          <Img src={image1.src} w="full" maxH="229px" />
-          <Img src={imgage2.src} w="full" maxH="319px" />
-          <Img src={image1.src} w="full" maxH="229px" />
+          <Skeleton w="full" maxH="229px">
+            <Img src={image1.src} w="full" maxH="229px" />
+          </Skeleton>
+          <Skeleton w="full" h="319px">
+            <Img src={image1.src} w="full" maxH="319px" />
+          </Skeleton>
+          <Skeleton w="full" maxH="229px">
+            <Img src={image1.src} w="full" maxH="229px" />
+          </Skeleton>
+          <Skeleton w="full" maxH="229px">
+            <Img src={image1.src} w="full" maxH="229px" />
+          </Skeleton>
+          <Skeleton w="full" maxH="319px">
+            <Img src={image1.src} w="full" maxH="319px" />
+          </Skeleton>
           <Flex
             sx={{
               flexWrap: 'wrap',
