6652e08079abe682bbea99b7116119bb0a009f88
paurock <tselenkor@gmail.com>
2022-07-21 10:24:17 +0300

    Tiles: add Link to Items

--- src/components/sections/content/Tiles/index.tsx
+++ src/components/sections/content/Tiles/index.tsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
-import { Flex, SimpleGrid, Skeleton } from '@chakra-ui/react';
+import { Flex, Link, SimpleGrid, Skeleton } from '@chakra-ui/react';
 import { Img } from 'shared';
 import image1 from '/src/app/assets/rectangle_240x229.png';
 import TileHover from './TileHover';
 import UnderTiles from './UnderTiles';
+import NextLink from 'next/link';
+import { NextRouter, useRouter } from 'next/router';
 
 export default function Tiles(): JSX.Element {
   const [isHover, setIsHover] = useState<boolean>(false);
+  const router: NextRouter = useRouter();
+  const url = router.pathname;
+  const urlNoLastEl = url.substring(0, url.lastIndexOf('/'));
+
   return (
     <Flex w="full" justifyContent="center">
       <Flex
@@ -15,21 +21,41 @@ export default function Tiles(): JSX.Element {
         maxW="1050px"
       >
         <SimpleGrid w="full" columns={[1, 2, 3, 4]} gap="30px" mt="24px">
-          <Skeleton w="full" maxH="229px">
-            <Img src={image1.src} w="full" maxH="229px" />
-          </Skeleton>
-          <Skeleton w="full" h="319px">
-            <Img src={image1.src} w="full" maxH="319px" />
-          </Skeleton>
-          <Skeleton w="full" maxH="229px">
-            <Img src={image1.src} w="full" maxH="229px" />
-          </Skeleton>
-          <Skeleton w="full" maxH="229px">
-            <Img src={image1.src} w="full" maxH="229px" />
-          </Skeleton>
-          <Skeleton w="full" maxH="319px">
-            <Img src={image1.src} w="full" maxH="319px" />
-          </Skeleton>
+          <NextLink href={urlNoLastEl + '/asset-id'} passHref>
+            <Link>
+              <Skeleton w="full" maxH="229px">
+                <Img src={image1.src} w="full" maxH="229px" />
+              </Skeleton>
+            </Link>
+          </NextLink>
+          <NextLink href={urlNoLastEl + '/asset-id'} passHref>
+            <Link>
+              <Skeleton w="full" h="319px">
+                <Img src={image1.src} w="full" maxH="319px" />
+              </Skeleton>
+            </Link>
+          </NextLink>
+          <NextLink href={urlNoLastEl + '/asset-id'} passHref>
+            <Link>
+              <Skeleton w="full" maxH="229px">
+                <Img src={image1.src} w="full" maxH="229px" />
+              </Skeleton>
+            </Link>
+          </NextLink>
+          <NextLink href={urlNoLastEl + '/asset-id'} passHref>
+            <Link>
+              <Skeleton w="full" maxH="229px">
+                <Img src={image1.src} w="full" maxH="229px" />
+              </Skeleton>
+            </Link>
+          </NextLink>
+          <NextLink href={urlNoLastEl + '/asset-id'} passHref>
+            <Link>
+              <Skeleton w="full" maxH="319px">
+                <Img src={image1.src} w="full" maxH="319px" />
+              </Skeleton>
+            </Link>
+          </NextLink>
           <Flex
             h="full"
             onMouseEnter={() => setIsHover(true)}
